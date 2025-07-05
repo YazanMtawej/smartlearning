244ae0fcd6996173fa64882c3d553c27490f0f2c
Yazan Mtawej <yznmtwj@gmail.com>
2025-07-05 16:10:21 +0300

    add urls.py in smartlearning folder by yazan mtawej

--- smartlearning/urls.py
+++ smartlearning/urls.py
@@ -1,15 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, EnrollmentViewSet, ContentViewSet, ExamViewSet, QuestionViewSet, ResultViewSet
-
-router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
-router.register(r'enrollments', EnrollmentViewSet)
-router.register(r'contents', ContentViewSet)
-router.register(r'exams', ExamViewSet)
-router.register(r'questions', QuestionViewSet)
-router.register(r'results', ResultViewSet)
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
 ]
